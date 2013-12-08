236f18f1f79d8565401bd4825eb6501e66fd5faf
montegava <montegava@gmail.com>
2013-12-08 23:51:01 +0200

    now it works

--- DAL/rental.Designer.cs
+++ DAL/rental.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'e:\.dropbox\Dropbox\rental\DAL\rentalEntities.edmx'. 
+﻿// Default code generation is disabled for model 'D:\Dropbox\rental\DAL\rental.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
