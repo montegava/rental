8d3879f996ec90826150fad18a3d17e6be1df0f8
montegava <montegava@gmail.com>
2014-01-12 00:17:22 +0200

    re #5 day comit

--- DAL/Flat/FlatManager.cs
+++ DAL/Flat/FlatManager.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-
-using System.Data;
-using MySql.Data.MySqlClient;
 using System.Linq.Expressions;
 using LinqKit;
 using log4net;
